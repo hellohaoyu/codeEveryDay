beb7a36634f49af3c05d999827b5ea4ea718ae69
hellohaoyu <haoyuatusa@gmail.com>
2018-03-13 23:38:33 -0700

    Update ValidateBST Solution

--- ValidateBST.py
+++ ValidateBST.py
@@ -55,6 +55,62 @@ class Solution(object):
             
         return True
 
+# It turns out I could pop visited note earlier!!
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        q = [root]
+        preVal = -sys.maxint - 1
+        visited = set()
+        
+        while q:
+            cur = q[-1]
+            if cur.left and cur.left not in visited:
+                q.append(cur.left)
+            else:
+                if preVal >= cur.val:
+                    return False
+                preVal = cur.val
+                visited.add(cur)
+                q.pop()
+                if cur.right:
+                    q.append(cur.right)
+            
+        return True    
+
+# Optimal solution to check cur is null or not, instead of left and right node.
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        q = []
+        preVal = -sys.maxint - 1
+        cur = root
+        
+        while q or cur is not None:
+            if cur is not None:
+                q.append(cur)
+                cur = cur.left
+            else:
+                check = q.pop()
+                if preVal >= check.val:
+                    return False
+                preVal = check.val
+                cur = check.right
+                  
+        return True            
+
 # Recursion method
 # Remember to pass max/min values for subtree!!
 class Solution(object):
